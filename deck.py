36e3d604c11b6fd4a7a4cd47470486c8774e7851
Philipp Martinek <Philipp.Martinek@bmw.de>
2020-02-14 18:16:56 +0100

    implement 3 player hand creation

--- deck.py
+++ deck.py
@@ -1,21 +1,34 @@
+import random
 from random import shuffle
 from typing import List, Iterable, Set
 
 from card_types import Card, Face, Suit, CARD_SCORE
 
+_random_instance = random.Random()
 
-def create_shuffled_deck() -> List[Card]:
+
+def initialize_seed(seed):
+    global _random_instance
+    _random_instance = random.Random(seed)
+
+
+def create_shuffled_deck(faces=Face) -> List[Card]:
     deck = []
-    for f in Face:
+    for f in faces:
         for s in Suit:
             deck.append(Card(s, f))
-    shuffle(deck)
+    _random_instance.shuffle(deck)
     return deck
 
 
-def create_shuffled_player_hands() -> List[Set[Card]]:
+def create_shuffled_4_player_hands() -> List[Set[Card]]:
     deck = create_shuffled_deck()
     return [set(deck[0:8]), set(deck[8:16]), set(deck[16:24]), set(deck[24:32])]
+
+
+def create_shuffled_3_player_hands() -> List[Set[Card]]:
+    deck = create_shuffled_deck(faces=[f for f in Face if f not in (Face.SIEBEN, Face.ACHT)])
+    return [set(deck[0:8]), set(deck[8:16]), set(deck[16:24])]
 
 
 def count_score(cards: Iterable[Card]):
